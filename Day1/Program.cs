263304ee064b31a6144bd233059d28c7b995f2c6
JamesStallworthy <james@jamesstallworthy.com>
2023-12-02 18:39:21 +0000

    Init commit

---
+++ Day1/Program.cs
@@ -0,0 +1,39 @@
+﻿Dictionary<string, char> Map = new Dictionary<string, char>(){
+ 	{"one", '1'},
+ 	{"two", '2'},
+ 	{"three", '3'},
+ 	{"four", '4'},
+ 	{"five", '5'},
+ 	{"six", '6'},
+ 	{"seven", '7'},
+ 	{"eight", '8'},
+ 	{"nine", '9'},
+};
+
+var file = File.ReadLines("./input.txt");
+
+var sum = 0;
+foreach (var line in file){
+	List<int> numbersInLine = new List<int>();
+	int index = 0;
+	foreach (char c in line){
+		if (char.IsNumber(c)){
+			numbersInLine.Add(c - '0');
+		}
+		else{
+			foreach(var strNum in Map){
+				if (line.Substring(index).StartsWith(strNum.Key)){
+					numbersInLine.Add(strNum.Value - '0');
+				}
+			}
+		}
+
+		index ++;
+	}
+
+	if (numbersInLine.Any()){
+		sum += int.Parse($"{numbersInLine.First()}{numbersInLine.Last()}");
+	}
+}
+
+Console.WriteLine($"Answer: {sum}");
---
+++ Day2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Day2{
+	class Set{
+		public int Red {get; set;}
+		public int Green {get; set;}
+		public int Blue {get; set;}
+
+		public void ParseLine(string set){
+			var cubes = set.Split(",");
+
+			foreach (var cube in cubes){
+				if (cube.Contains(" red")){
+					Red = int.Parse(cube.Replace(" red", ""));
+				}
+				else if (cube.Contains(" green")){
+					Green = int.Parse(cube.Replace(" green", ""));
+				}
+				else if (cube.Contains(" blue")){
+					Blue = int.Parse(cube.Replace(" blue", ""));
+				}
+			}
+		}
+
+		public bool IsPossible(int r, int g, int b){
+			return Red <= r && Green <= g && Blue <= b;
+		}
+	}
+
+	class Game{
+		List<Set> Sets = new List<Set>();
+
+		//Min needed
+		private int MinRed {get => Sets.Select(x => x.Red).Max();}
+		private int MinGreen {get => Sets.Select(x => x.Green).Max();}
+		private int MinBlue {get => Sets.Select(x => x.Blue).Max();}
+
+		public void ParseLine(string line){
+			//Trim the game part
+			line = line.Split(':')[1];
+
+			var sets = line.Split(";");
+
+			foreach (var setStr in sets){
+				Set newSet = new Set();
+
+				newSet.ParseLine(setStr);
+
+				Sets.Add(newSet);
+			}
+		}
+
+		public bool IsPossible(int r, int g, int b){
+			foreach (var set in Sets){
+				if (!set.IsPossible(r, g, b)){
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public int Power(){
+			return MinRed * MinGreen * MinBlue;
+		}
+	}
+
+	class Program
+	{
+		static List<Game> Games = new List<Game>();
+
+		static void Main(string[] args)
+		{
+			var file = File.ReadLines("./input.txt");
+
+			foreach (var line in file){
+				Game newGame = new Game();
+
+				newGame.ParseLine(line);
+
+				Games.Add(newGame);
+			}
+
+			int sum = 0;
+			int sumOfPower = 0;
+			for	(int i = 0; i < Games.Count; i++){
+				if (Games[i].IsPossible(12,13,14)){
+					sum += (i + 1);
+				}
+
+				sumOfPower += Games[i].Power();
+			}
+
+			Console.WriteLine($"Sum of possible {sum}");
+			Console.WriteLine($"Sum of power {sumOfPower}");
+		}
+	}
+}
