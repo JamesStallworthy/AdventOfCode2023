7f7092349175e7ebac21ff345038b443e6cd31ae
JamesStallworthy <james@jamesstallworthy.com>
2023-12-17 00:29:21 +0000

    Added timer to rust tests

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,6 +3,15 @@ mod day2;
 mod utils;
 
 fn main() {
-    day1::solve();
-    day2::solve();
+    run_day("Day1", day1::solve);
+    run_day("Day2", day2::solve);
+}
+
+fn run_day(day: &str, f: fn()) {
+    println!("{}", day);
+    let now = std::time::Instant::now();
+
+    f();
+
+    println!("Took: {}ms", now.elapsed().as_millis());
 }
